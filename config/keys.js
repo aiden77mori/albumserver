ffb3fb25c90b0cbf86a478222dd64d766a97ec84
mastercodingbear <mastercodingbear@gmail.com>
2021-11-01 02:28:09 +0300

    heroku database url change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     postgreURI:
-       'postgres://postgres:hello@21247@localhost:5432/albumApp',
+      //  'postgres://postgres:hello@21247@localhost:5432/albumApp',
+      'postgres://xkrajwckenphph:dea3294b0c3ad1e3ec2874e8b6fb888b85d51af5d0a9b37572be67099aef57c1@ec2-34-203-91-150.compute-1.amazonaws.com:5432/dbpt7jsfgb4kt1',
     secretOrKey: 'secret'
  }

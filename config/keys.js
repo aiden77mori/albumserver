30d41cbe3351d1e0594d5061677a89343271e767
mastercodingbear <mastercodingbear@gmail.com>
2021-11-03 03:10:36 +0300

    change

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     postgreURI:
       //  'postgres://postgres:hello@21247@localhost:5432/albumApp',
-      'postgres://rwelunwxlbjtwd:f2ee9e3b76691d8671defeff90177265582d4482928652c06dc14a00a662e722@ec2-54-224-142-15.compute-1.amazonaws.com:5432/detb7121ldrk1'
+      'postgres://rwelunwxlbjtwd:f2ee9e3b76691d8671defeff90177265582d4482928652c06dc14a00a662e722@ec2-54-224-142-15.compute-1.amazonaws.com:5432/detb7121ldrk1',
     secretOrKey: 'secret'
  }

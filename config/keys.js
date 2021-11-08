bf68f4f3442a01ed2c4391ebde9f42f662e1a979
mastercodingbear <mastercodingbear@gmail.com>
2021-11-09 01:13:04 +0200

    heroku postgre database

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     postgreURI:
       //  'postgres://postgres:hello@21247@localhost:5432/albumApp',
-      'postgres://rwelunwxlbjtwd:f2ee9e3b76691d8671defeff90177265582d4482928652c06dc14a00a662e722@ec2-54-224-142-15.compute-1.amazonaws.com:5432/detb7121ldrk1',
+      'postgres://ennslhsxyloapb:4d44ba16ea738bc786da1af12912db1a0fda4a30a5807885d010e549923d8833@ec2-3-218-92-146.compute-1.amazonaws.com:5432/d8o4brc9golu6o',
     secretOrKey: 'secret'
  }
